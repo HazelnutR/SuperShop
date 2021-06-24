882822dd5b3559cba571fa97852ab03ce91771b8
HazelnutR <ruiduarte987@gmail.com>
2021-06-24 10:35:47 +0100

    Modify Products.

---
+++ WebApplication2/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data.Entities;
+
+namespace WebApplication2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
